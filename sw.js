06401aad82bde8b2f39b771ca94b08eec79e9280
zoror2993-eng <zoror2993@gmail.com>
2026-02-21 12:56:37 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+      self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
